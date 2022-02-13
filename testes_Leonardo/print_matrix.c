1bfe19b58dc3f361c5f42be12e01460a98605880
asoler <asoler@student.42sp.org.br>
2022-02-13 22:56:56 +0100

    print matrix

--- testes_Leonardo/print_matrix.c
+++ testes_Leonardo/print_matrix.c
@@ -1,37 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_matrix.c                                  :+:      :+:    :+:   */
+/*   print_matrix.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 23:06:52 by lledo-da          #+#    #+#             */
-/*   Updated: 2022/02/13 21:34:13 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/13 22:53:49 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_matrix();
-//[TODO]
-// char *retorna uma array inicializado q vamos modificar para printar numeros de 1 a 4
-// usar uma funçãmo maloqui para poder fazer uma matriz maior q 4.
-// ou talves uma função auxiliar recursiva para converter char en int
-void print_matrix(int size)
+	// char **process_matr(char **matrix)
+	// {
+	// 	matrix[4][4];
+	// 	int row;
+	// 	int col;
+		
+	// 	row = 0;
+	// 	col = 0;
+		
+		
+	// }
+
+void print_matrix(void)
 {
-	char matrix[size][size];
+	char matrix[4][4];
+	// process_matr(matrix);
 	int row;
 	int col;
 	
 	row = 0;
 	col = 0;
-	while(row <= (size - 1))
+	while(row <= 3)
 	{
-		while(col <= (size - 1))
+		while(col <= 3)
 		{
-			matrix[row][col] = 'x';
+			matrix[row][col] = '0';
 			write(1, &matrix[row][col], 1);
-			if (col != (size - 1))
+			if (col != 3)
 				write(1, " ", 1);
 			col++;
 		}
