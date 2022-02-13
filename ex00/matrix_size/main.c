a3f37565fcb5bc38ba012e6204fcba7206638e21
asoler <asoler@student.42sp.org.br>
2022-02-13 22:28:41 +0100

    isolando programinha de printar matriz size + tirando espaços de argv e mandado error quando é maior a 31

--- ex00/matrix_size/main.c
+++ ex00/matrix_size/main.c
@@ -6,60 +6,16 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 23:06:16 by lledo-da          #+#    #+#             */
-/*   Updated: 2022/02/13 22:26:24 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/13 22:20:16 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// void print_matrix(int size);
-#include <stdio.h>
-#include <unistd.h>
-
-char *split(char *aux, char *s)
-{
-	int i;
-	int index;
-	i = 0;
-	index = 0;
-
-	while (i <= 30)
-	{
-		if (i % 2 == 0)
-		{
-			aux[index] = s[i];
-			index++;
-		}
-		i++;
-	}
-	aux[index] = '\0';
-	return (aux);
-}
-
-int count(char *s)
-{
-	int i = 1;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
+void print_matrix(int size);
 
 int main(int argc, char *argv[])
 {
-	char	char_pro[17];
-	int		n;
-	char *error;
+	int x;
 
-	error = "Error\n";
-
-	n = count(argv[argc - 1]);
-	
-	if (n < 31 || n > 31)
-		write(1, error, 6);
-	else
-	{
-		split(char_pro, argv[argc - 1]);
-		printf("%s\n", char_pro);
-	}
+	x = *argv[argc - 1] - 48;
+	print_matrix(x);
 }
