21c081aa01e4b4f6f321a75752ab41c607a990a8
asoler <asoler@student.42sp.org.br>
2022-02-14 02:05:42 +0100

    final commit

--- ex00/processamento_de_parametro.c
+++
@@ -1,6 +0,0 @@
-
-
-char *processamento_de_parametro(char *dest, char *arg) //como receber um array de arrays(multidimensional) em função e arrays (unidimensional?
-{
-	dest[4][4]
-}
--- testes_Leonardo/split_parameters.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-
-void split_parameters(char *argv)
-{
-	write(1, "Ok", 1);
-}
